91a7f25bd1f4068b8d75446ecce4f4753c4d9f17
Trevor Scheitrum <tascheitrum@gmail.com>
2014-04-22 04:14:12 -0400

    commented out some code from offsite_stripe.py because of errors being thrown

--- shop_stripe/offsite_stripe.py
+++ shop_stripe/offsite_stripe.py
@@ -3,14 +3,15 @@ from django import forms
 from django.conf import settings
 from django.conf.urls.defaults import patterns, url
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
-from shop.util.decorators import on_method, shop_login_required
 from django.forms.forms import DeclarativeFieldsMetaclass
-from django.http import HttpResponseBadRequest, HttpResponse, HttpResponseRedirect
-from django.template import RequestContext
+from django.http import HttpResponseBadRequest, HttpResponse, \
+    HttpResponseRedirect, HttpResponseRedirect
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+from shop.util.decorators import on_method, shop_login_required
 from shop_stripe.forms import CardForm
 import stripe
+from django.shortcuts import redirect
 
 class ConfigError(Exception):
     def __init__(self, value):
@@ -23,7 +24,7 @@ class StripeBackend(object):
     A django-shop payment backend for the stripe service, this 
     is the workhorse view. It processes what the CardForm class
     kicks back to the server.
-    
+
     It also saves the customer billing information for later use.
     '''
     backend_name="Stripe"
@@ -43,55 +44,64 @@ class StripeBackend(object):
 
     def stripe_payment_view(self, request):
         if request.POST:
-            if request.user.is_authenticated() and not request.user.get_profile().stripe_customer_id == '':
-                customer_id = request.user.get_profile().stripe_customer_id 
-            else:
-                customer_id=None
-                card_token = request.POST['stripeToken']
+            #if request.user.is_authenticated() and request.user.get_profile().stripe_customer_id:
+            #    customer_id = request.user.get_profile().stripe_customer_id 
+            #else:
+            #    card_token = request.POST['stripeToken']
+            card_token = request.POST['stripeToken']
             order = self.shop.get_order(request)
             order_id = self.shop.get_order_unique_id(order)
             amount = self.shop.get_order_total(order)
             amount = str(int(amount * 100))
-            if request.user.is_authenticated():
-                description = request.user.email
-            else:
-                description = 'guest customer'
-            
+            if request.user.is_authenticated(): description = request.user.email
+            else: description = 'guest customer'
+
             currency = getattr(settings, 'SHOP_STRIPE_CURRENCY', 'usd') # Default to good ol' U.S. Dollar
-            
+
             if hasattr(settings, 'SHOP_STRIPE_PRIVATE_KEY'):
                 stripe.api_key=settings.SHOP_STRIPE_PRIVATE_KEY
             else: 
                 raise ConfigError('You must set SHOP_STRIPE_PRIVATE_KEY in your configuration file.')
+
+
+            stripe_dict = {
+                'amount':amount,
+                'currency':currency,
+                'card':card_token,
+                'description':description,}
             
-            if not customer_id:
-                stripe_dict = {
-                    'amount':amount,
-                    'currency':currency,
-                    'card':card_token,
-                    'description':description,}
-            else:
-                stripe_dict = {
-                    'amount':amount,
-                    'currency':currency,
-                    'customer':customer_id,
-                    'description':description,}
+            #if not customer_id:
+            #    stripe_dict = {
+            #        'amount':amount,
+            #        'currency':currency,
+            #        'card':card_token,
+            #        'description':description,}
+            #else:
+            #    stripe_dict = {
+            #        'amount':amount,
+            #        'currency':currency,
+            #        'customer':customer_id,
+            #        'description':description,}
             
-            stripe_result = stripe.Charge.create(**stripe_dict)
-            self.shop.confirm_payment(self.shop.get_order_for_id(order_id), amount, stripe_result['id'], self.backend_name)
+            stripeCharge = stripe.Charge.create(**stripe_dict)
+            
+            transaction_id = stripeCharge.id
+
+            self.shop.confirm_payment(self.shop.get_order_for_id(order_id), amount, transaction_id, self.backend_name) #, transaction_id, self.backend_name)
 
             # If we're logged in, save the transaction token for use again later. Sweet.
-            if request.user.is_authenticated and request.user.get_profile().stripe_customer_id == None: 
-                customer = stripe.Customer.create(card=card_token, description=description)
-                profile = request.user.get_profile()
-                profile.stripe_customer_id = customer.id
-                profile.save()
+            #if request.user.is_authenticated and request.user.get_profile().stripe_customer_id == None: 
+            #    customer = stripe.Customer.create(card=card_token, description=description)
+            #    request.user.get_profile().stripe_customer_id=customer.id
+            #    request.user.save()
+            return redirect('/shop/checkout/thank_you')
              
         if hasattr(settings, 'SHOP_STRIPE_PUBLISHABLE_KEY'):
             pub_key=settings.SHOP_STRIPE_PUBLISHABLE_KEY
         else: 
             raise ConfigError('You must set SHOP_STRIPE_PUBLISHABLE_KEY in your configuration file.')
-        form = CardForm
+        
+        form = CardForm(forms.Form)
         context = RequestContext(request, {'form':form, 'STRIPE_PUBLISHABLE_KEY':pub_key})
         return render_to_response("shop_stripe/payment.html", context)
 
